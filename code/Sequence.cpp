09a1503591db73b653cdd7018e07287ebecb3a7a
jedikarix <karolchecinski@wp.pl>
2019-12-12 17:46:35 +0100

    Implement sequence_utility function

--- code/Sequence.cpp
+++ code/Sequence.cpp
@@ -8,6 +8,13 @@ inline unsigned item_count(const Item& item, const Transaction& transaction) {
     return transaction.count(item) != 0 ? transaction.at(item) : 0;
 }
 
+unsigned transaction_utility(const Transaction& transaction, const ProfitTable& profit_table) {
+    unsigned utility = 0;
+    for (auto const& item : transaction)
+        utility += item.second * profit_table.at(item.first);
+    return utility;
+}
+
 unsigned utility_of_item(const Item& item, const Sequence& seq, const ProfitTable& profit_table) {
     unsigned max_util = 0;
     for (const Transaction& t : seq) {
@@ -16,4 +23,13 @@ unsigned utility_of_item(const Item& item, const Sequence& seq, const ProfitTabl
             max_util = utility;
     }
     return max_util;
+}
+
+
+unsigned sequence_utility(const Sequence& seq, const ProfitTable& profit_table) {
+    unsigned utility = 0;
+    for (const Transaction& t : seq) {
+        utility += transaction_utility(t, profit_table);
+    }
+    return utility;
 }
--- code/Sequence.h
+++ code/Sequence.h
@@ -18,10 +18,11 @@ typedef std::list<Sequence> SDB;
 
 inline bool has_item(const Item& item, const Transaction& transaction);
 inline unsigned item_count(const Item& item, const Transaction& transaction);
+unsigned transaction_utility(const Transaction& transaction, const ProfitTable& profit_table);
 unsigned utility_of_item(const Item& item, const Sequence& seq, const ProfitTable& profit_table);
 unsigned utility_of_subsequence(const Sequence& subseq, const Sequence& seq, const ProfitTable& profit_table);
 unsigned actual_sequence_utility(const Sequence& seq, const SDB& sdb, const ProfitTable& profit_table);
-unsigned sequence_utility(const Sequence& seq, const ProfitTable& profitTable);
+unsigned sequence_utility(const Sequence& seq, const ProfitTable& profit_table);
 unsigned sequence_utility_upper_bound(const Sequence& seq, const SDB& sdb, const ProfitTable& profit_table);
 
 #endif //PHUS_EDAMI_SEQUENCE_H
